7379d70483f1725d1d3ba028f2e0a538b7b425b0
senthil-arch <senthil.santhoshgopal@gmail.com>
2025-12-28 15:26:27 +1300

    remove id from response

--- models.go
+++ models.go
@@ -3,7 +3,12 @@ package main
 import "time"
 
 type Entry struct {
-	ID      uint      `json:"id" gorm:"primaryKey"`
+	ID      uint      `json:"-" gorm:"primaryKey"` // Don't serialize ID in JSON
+	Date    time.Time `json:"date"`
+	Visited bool      `json:"visited"`
+}
+
+type EntryResponse struct {
 	Date    time.Time `json:"date"`
 	Visited bool      `json:"visited"`
 }
